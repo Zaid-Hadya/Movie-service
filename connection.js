9bd8ff7943d7e2078d18b03d06fb8263b62f5431
Zaid <zaid.omar962@gmail.com>
2024-05-22 09:11:35 +0400

    Add the schema and a model in connection.js

--- connection.js
+++ connection.js
@@ -1,14 +1,31 @@
+import mongoose from 'mongoose';
+import express from 'express';
 
-const mongoose = require('mongoose');
-
+const app = express();
+const port = 3000
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/MovieStore')
-  .then(() => {
-    console.log('Connection has been made...');
-  })
-  .catch((error) => {
+mongoose.connect('mongodb://localhost:27017/MovieStore').then(() => {
+    console.log('Connection has been made...')
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+  }).catch((error) => {
     console.log('Connection error:', error);
   });
 
-  console.log('Test');
+  const movieSchema = new mongoose.Schema({
+    title: String,
+    release_date: String,
+  });
+
+
+  // Create a model from the schema
+const MovieModal = mongoose.model('Movies', movieSchema)
+
+app.get("/getMovies", async(req, res) =>{
+  const moviesData = await MovieModal.find();
+  res.json(moviesData)
+});
+
+  
